e82e9175d26a0f02d00947156c15416df12753d9
SebastianCordova473 <sebas-12359@outlook.es>
2024-07-17 19:41:25 -0500

    renaming variable

--- src/app/dashboard/services/ventas.service.ts
+++ src/app/dashboard/services/ventas.service.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-
+import {Global} from 'app/global';
 @Injectable({
   providedIn: 'root',
 })
 export class VentasService {
-  private apiUrl = 'https://franz.kvconsult.com/fapi-dev/api.php/ventas/total';
+  private apiUrl = Global.BASE_API_URL + '/api.php/ventas/total';
 
   constructor(private http: HttpClient) {}
 
