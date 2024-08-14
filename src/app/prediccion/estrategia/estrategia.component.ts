ef242a732b9b82c8a85d6ab79ce22f95f6ff5621
SebastianCordova473 <sebas-12359@outlook.es>
2024-08-13 20:56:40 -0500

    reduccion y estrategias

--- src/app/prediccion/estrategia/estrategia.component.ts
+++ src/app/prediccion/estrategia/estrategia.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from './data.service';
+import {StrategyService} from './strategy.service';
 
 @Component({
   selector: 'app-estrategia',
   templateUrl: './estrategia.component.html',
-  styleUrls: ['./estrategia.component.css']
+  styleUrls: ['./estrategia.component.css'],
 })
-export class EstrategiaComponent {
+export class EstrategiaComponent implements OnInit {
+  data: any;
+  strategies: any[] = [];
+  hasEffectiveStrategies: boolean = false;
 
+  constructor(
+    private dataService: DataService,
+    private strategyService: StrategyService
+  ) {}
+
+  ngOnInit(): void {
+    this.dataService.fetchData().subscribe(
+      (data) => {
+        this.data = {
+          wastes: data.waste,
+          sales: data.sales,
+          demands: data.demands,
+          offers: data.offers,
+          products: data.productos,
+          season: data.season,
+          categories: data.categorias,
+          subCategories: data.subCategorias,
+        };
+
+        this.strategies = this.strategyService
+          .recommendStrategies(this.data)
+          .filter((strategy) => strategy.improvementPercentage >= 10);
+
+        this.hasEffectiveStrategies = this.strategies.length > 0;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 }
